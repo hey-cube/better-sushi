78eac567b3b3387000665566fc544b8c6007cbae
hey-cube <tales.phan.x@gmail.com>
2019-09-03 00:01:15 +0900

    :sushi: :sushi: :sushi:

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "better-sushi" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "better-sushi" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.makeEverythingSushi", () => {
+      const { activeTextEditor } = vscode.window;
+      if (activeTextEditor !== undefined) {
+        const { document } = activeTextEditor;
+        const startPosition = new vscode.Position(0, 0);
+        const endPosition = new vscode.Position(document.lineCount - 1, 10000);
+        const selection = new vscode.Selection(startPosition, endPosition);
+        const text = document.getText(selection);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+        activeTextEditor.edit(edit => {
+          edit.replace(
+            selection,
+            text.replace(/[^ -/:-@\{-~　\t\n\\🍣]/g, "🍣")
+          );
+        });
+      }
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
